07fe8fa341860135bd01d6d15b1b1ea93daf9f0c
Cyph <45281452+imcyph@users.noreply.github.com>
2024-09-07 11:28:43 +1000

    Update constants.ts

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,5 +1,5 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
-export const DISCORD_LINK = "https://discord.gg/R6VneQvK";
+export const DISCORD_LINK = "https://discord.gg/fg8Z6rKJtU";
 export const GITHUB_LINK = "https://github.com/imcyph/smov";
 export const TWITTER_LINK = "https://twitter.com/imcyph";
 export const GA_ID = import.meta.env.VITE_GA_ID;
